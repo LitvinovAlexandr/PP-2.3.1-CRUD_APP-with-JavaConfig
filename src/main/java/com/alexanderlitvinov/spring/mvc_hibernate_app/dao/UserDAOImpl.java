24e6ca6ee1dcf91ecbfa943fa88954232502b27c
LitvinovAlexandr <sanlitv56@gmail.com>
2024-04-21 23:39:55 +0500

    Task PP-2.3.1 is done

--- src/main/java/com/alexanderlitvinov/spring/mvc_hibernate_app/dao/UserDAOImpl.java
+++ src/main/java/com/alexanderlitvinov/spring/mvc_hibernate_app/dao/UserDAOImpl.java
@@ -3,50 +3,40 @@ package com.alexanderlitvinov.spring.mvc_hibernate_app.dao;
 import com.alexanderlitvinov.spring.mvc_hibernate_app.entity.User;
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public List<User> getAllUsers() {
-        Session session=  sessionFactory.getCurrentSession();
-//        List<User> allUsers =session.createQuery("from User"
-//                        , User.class).getResultList();
-
-        Query<User> query = session.createQuery("from User"
-                        , User.class);
-        List<User> allUsers = query.getResultList();
-        return allUsers;
+        Query query = (Query) entityManager.createQuery("from User ", User.class);
+        return query.getResultList();
     }
 
     @Override
     public void saveUser(User user) {
-        Session session = sessionFactory.getCurrentSession();
-        session.saveOrUpdate(user);
+        entityManager.merge(user);
     }
 
     @Override
     public User getUser(int id) {
-        Session session = sessionFactory.getCurrentSession();
-        User user = session.get(User.class, id);
-        return user;
+        return entityManager.find(User.class, id);
     }
 
     @Override
     public void deleteUser(int id) {
-        Session session = sessionFactory.getCurrentSession();
-        Query<User> query = session.createQuery("delete from User " +
-                                                "where id =:userId");
+        Query query = (Query) entityManager.createQuery("delete from User " +
+                                                        "where id =:userId");
         query.setParameter("userId", id);
         query.executeUpdate();
     }
